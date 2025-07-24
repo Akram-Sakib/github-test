f2c71b0d69fd28cb2ad0db5c03c3418b8c790e94
akram-sakib <sayedakramsakib@gmail.com>
2025-07-24 13:06:22 +0600

    Update greetings in Home component to new content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
 
   return (
     <div className="flex items-center justify-center flex-col h-screen">
-      <div className="text-4xl font-bold">Hello World</div>
-      <div className="text-4xl font-bold">Hello World 2</div>
+      <div className="text-4xl font-bold">New Commit 1</div>
+      <div className="text-4xl font-bold">New Commit 2</div>
     </div>
   );
 }
