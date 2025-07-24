be446e28cc59449e9e755467156d83294b04df60
akram-sakib <sayedakramsakib@gmail.com>
2025-07-24 11:40:34 +0600

    Add second greeting to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center flex-col h-screen">
       <div className="text-4xl font-bold">Hello World</div>
+      <div className="text-4xl font-bold">Hello World 2</div>
     </div>
   );
 }
